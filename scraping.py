dc6d5de86a777ffe97fd0b5b7e08c00cf8d6501e
Alexis Cousin <alexistyu1@gmail.com>
2021-09-25 17:11:06 +0200

    ajout de l'extraction de plusieurs données d'un livre

--- scraping.py
+++ scraping.py
@@ -1,7 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
-url="https://books.toscrape.com/index.html"
+url="https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
-
-
+availability=soup.find("p",class_="instock availability")
+price=soup.find("p",class_="price_color")
+description=soup.find_all("p")[-1]
+title=soup.find("h1")
+productInformation=soup.find("tr",class_="table table-striped")
+#productUrl=
+#upc=
+#priceIncludingTax=
+#priceExcludingTax=
+#NumberAvailable=
+#category=
+#reviewRating=
+#imageUrl=soup.find("div",class_="item active")
+#print (title.string)
+#print (price.string)
+#print(availability.string)
+#print(description.string)
+#print(imageUrl.string)
+print(productInformation)
