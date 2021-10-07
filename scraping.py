303598b1226bf25f65001691b5e547bc38057eda
Alexis Cousin <alexistyu1@gmail.com>
2021-10-07 02:41:34 +0200

    division du fichier csv en fonction des categories et correction des dernier bugs sur la fonction product

--- scraping.py
+++ scraping.py
@@ -31,34 +31,31 @@ def main(homeUrl):
         categoriesForUrl.append(category)
         categories.append(i.string.strip())
         number += 1
-    print(categoriesForUrl)
-    print(categories)
     booksUrlList = []
     number = 1
     en_tete = ["product_page_url", "universal_product_code", "title", "price_including_tax", "price_excluding_tax",
                "number_available", "product_description", "category", "review_rating", "image_url"]
-    with open('product.csv', "w", newline="") as file_csv:
-        writer = csv.writer(file_csv, delimiter=",")
-        writer.writerow(en_tete)
     for i in categoriesForUrl[1:]:
+        with open(categories[number]+".csv", "w", newline="") as file_csv:
+            writer = csv.writer(file_csv, delimiter=",")
+            writer.writerow(en_tete)
         url = "https://books.toscrape.com/catalogue/category/books/"+i+"/index.html"
         getBooksUrl(url, booksUrlList, i)
         for y in booksUrlList:
             book = product(y, categories[number])
-            with open('product.csv', "a", newline="") as file_csv:
+            with open(categories[number]+".csv", "a", newline="", encoding='utf-8') as file_csv:
                 writer = csv.writer(file_csv, delimiter=",")
                 writer.writerow([book.product_page_url, book.universal_product_code, book.title, book.price_including_tax, book.price_excluding_tax, book.number_available, book.product_description, book.category, book.review_rating, book.image_url])
         booksUrlList = []
+        print(categories[number]+" is done")
         number += 1
 
 
 def product(url, category):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
-    description = soup.find_all("div", class_=False)
     title = soup.find("h1").string
     productInformation = soup.find_all("td")
-    productUrl = url
     upc = productInformation[0].string
     priceIncludingTax = productInformation[3].string
     priceExcludingTax = productInformation[2].string
@@ -74,26 +71,13 @@ def product(url, category):
         reviewRating = 4
     else:
         reviewRating = 5
-    print(reviewRating)
-    imageUrl = soup.find("img")
-    print("upc = " + upc)
-    print("title = " + title)
-    print("price excluding taxes =" + priceExcludingTax)
-    print("price Including taxes =" + priceIncludingTax)
+    imageUrl = "https://books.toscrape.com/"+soup.find("img")["src"].replace("../../", "")
     if availability[:8] == "In stock":
         numberAvailable = availability[10:-11]
     else:
         numberAvailable = 0
-    print(numberAvailable)
-    print("url of this product is:" + productUrl)
-    print(category)
-    # print(description)
-    # print(imageUrl)
-    # print(imageUrl.string)
-    # print(type(imageUrl))
-    # print(imageUrl.get('href'))
-    # print(description[3].contents)
-    book = Book(url, upc, title, priceExcludingTax, priceIncludingTax, numberAvailable, "description", category, reviewRating, "imageUrl")
+    description = soup.find_all("meta")[2]["content"].strip()
+    book = Book(url, upc, title, priceExcludingTax, priceIncludingTax, numberAvailable, description, category, reviewRating, imageUrl)
     return book
 
 
@@ -123,4 +107,3 @@ def getBooksUrl(url, booksUrlList, category):
 main(homePageUrl)
 # product("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html", "travel")
 test = Book("product_page_url", "universal_product_code", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url")
-
